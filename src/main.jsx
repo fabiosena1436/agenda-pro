a5df593a533366b123e154be91e77179564a6fdf
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2025-10-18 20:21:04 -0300

    att

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyles } from './styles/GlobalStyles';
 import { AuthProvider } from './contexts/AuthContext'; // <-- IMPORTA AQUI
 import 'react-datepicker/dist/react-datepicker.css';
+import { ptBR } from 'date-fns/locale';
+import { registerLocale, setDefaultLocale } from 'react-datepicker';
 
 // MUI imports
 import { ThemeProvider } from '@mui/material/styles';
@@ -13,10 +15,13 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './styles/theme';
 
+registerLocale('pt-BR', ptBR);
+setDefaultLocale('pt-BR');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ptBR}>
         <CssBaseline />
         <AuthProvider> {/* <-- ADICIONA O AUTHPROVIDER AQUI */}
           <BrowserRouter>
